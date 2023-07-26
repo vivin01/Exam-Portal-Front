d37f55673872577f7482627a57310e7159fca34a
vivin01 <vivinmevada01@gmail.com>
2023-07-26 17:55:10 +0530

    commit msg

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/app/services/user.service';
 import swal from 'sweetalert2';
@@ -8,7 +8,7 @@ import swal from 'sweetalert2';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
   constructor(private userService: UserService,) { }
 
@@ -16,10 +16,10 @@ export class SignupComponent {
     // id:"",
     username: "",
     password: "",
-    email: "",
-    phone: "",
     firstName: "",
     lastName: "",
+    email: "",
+    phone: "",
 
   }
   ngOnInit(): void { }// when our program run then this will execute first
